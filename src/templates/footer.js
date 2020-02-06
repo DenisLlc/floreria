de8b4513693c9c6e8eadd66d78270f940f9f8d29
DenisLlc <dllamoca665@gmail.com>
2020-02-06 11:20:03 -0500

    footer para todos

---
+++ src/templates/footer.js
@@ -0,0 +1,43 @@
+document.write('\
+    <footer>\
+        <section class="column column-1">\
+            <b>Nosotros</b>\
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, amet laborum. Enim dolorum nam aliquam molestias, cumque numquam consequuntur eligendi, deleniti vel nesciunt nihil quae voluptas, eius quidem deserunt sapiente?</p>\
+        </section>\
+        <section class="column">\
+            <b>Navegación</b>\
+            <ul>\
+                <li><a href="index.html">Inicio</a></li>\
+                <li><a href="products.html">Productos</a></li>\
+                <li><a href="ocasiones.html">Ocasiones.html</a></li>\
+                <li><a href="servicios.html">Servicios</a></li>\
+                <li><a href="contact.html">Contáctanos</a></li>\
+                <li><a href="">Carrito</a></li>\
+            </ul>\
+        </section>\
+        <section class="column">\
+            <b>Nosotros</b>\
+            <ul>\
+                <li><a href="">Se parte de nosotros</a></li>\
+                    <li><a href="">Iniciar sesión</a></li>\
+            </ul>\
+        </section>\
+        <section class="column">\
+            <b>Redes sociales</b>\
+            <ul>\
+                <li>\
+                    <span>Facebook</span>\
+                    <a href="#" class="btn-social" id="btn-facebook"><i class="fab fa-facebook-f"></i></a>\
+                </li>\
+                <li>\
+                    <span>Instagram</span>\
+                    <a href="#" class="btn-social" id="btn-instagram"><i class="fab fa-instagram"></i></a>\
+                </li>\
+                <li>\
+                    <span>Pinterest</span>\
+                    <a href="#" class="btn-social" id="btn-pinterest"><i class="fab fa-pinterest"></i></a>\
+                </li>\
+            </ul>\
+        </section>\
+    </footer>\
+');
